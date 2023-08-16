a1636b0a8eda46a00ec6884e0b196756b68a23fe
Jaskeerat Singh <jaskeerat10000@gmail.com>
2023-08-17 00:40:10 +0530

    feat: 🎥 Completed Video Generation using Replicate AI

--- application/components/empty.tsx
+++ application/components/empty.tsx
@@ -10,8 +10,7 @@ export const Empty = ({
   return (
     <div className="h-full p-20 flex flex-col items-center justify-center">
       <div className="relative h-72 w-72">
-      
-        <Image src="/empty.gif" fill alt="Empty" />
+        <Image src="/empty.gif" fill alt="Empty" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" />
       </div>
       <p className="text-muted-foreground text-sm text-center">
         {label}
